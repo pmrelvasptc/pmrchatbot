43c6f3c3fe806250f78b48b4b47ff9e21f9ddef5
Pedro Relvas <pedro.relvas@jumia.com>
2021-03-04 18:47:53 +0000

    project setup

---
+++ pmr-chatbot-backend/src/main/java/com/pmrelvas/pmrchatbot/PmrchatbotApplication.java
@@ -0,0 +1,13 @@
+package com.pmrelvas.pmrchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmrchatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmrchatbotApplication.class, args);
+	}
+
+}
---
+++ pmr-chatbot-backend/src/test/java/com/pmrelvas/pmrchatbot/PmrchatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmrelvas.pmrchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmrchatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
